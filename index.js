f13fc092745591f310b37b22d057f3b733649dc9
Abby Lortz <abilenelortz03@gmail.com>
2018-06-22 20:52:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function scuberGreetingForFeet(distance) {
   }
 }
 function ternaryCheckCity(city) {
-  if (city = 'NYC' && city >= 'Pittsburgh') {
+  if (city = 'NYC' && city = 'Pittsburgh') {
     return 'Ok, sounds good.'
   }
   else if (city = 'Pittsburgh') {
