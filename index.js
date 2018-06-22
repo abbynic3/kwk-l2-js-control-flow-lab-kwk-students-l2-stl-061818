a875603ce7bcdc821185f4488a4dedbba6ad2780
Abby Lortz <abilenelortz03@gmail.com>
2018-06-22 21:24:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your code in this file!
+
+
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return 'This one is on me!'
+  }
+  else if (distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (distance > 2500) {
+    return 'No can do.'
+  }
+}
+
+function ternaryCheckCity(city) {
+  if (city == 'NYC') {
+    return 'Ok, sounds good.'
+  }
+  else if (city == 'Pittsburgh') {
+    return 'No go.'
+  }
+}
+
+function switchOnCharmFromTip(tip) {
+  if (tip == 'generous') {
+    return 'Thank you so much.' 
+    }
+  else if (tip == 'not as generous') {
+    return 'Thank you.'
+  }
+  else if (tip == 'thanks for everything') {
+    return 'Bye.'
+  }
+}
+
